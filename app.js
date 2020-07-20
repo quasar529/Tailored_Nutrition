472dab7da316cc447abbd5968f424651dfd4cbb7
quasar529 <quasar0529@gmail.com>
2020-07-20 14:16:47 +0900

    show 3 cooks with title,ingredients and simple recipe

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 async function getCook(string) {
   try {
     let results = await fetch(
-      `https://api.spoonacular.com/recipes/findByNutrients?apiKey=9ef8ed83e4164a2eb14cf5bff7669c9a&${string}&random&number=5`
+      `https://api.spoonacular.com/recipes/findByNutrients?apiKey=9ef8ed83e4164a2eb14cf5bff7669c9a&${string}&random&number=3`
     );
     let data = await results.json();
     return data;
@@ -9,13 +9,14 @@ async function getCook(string) {
     console.log(error);
   }
 }
+
 async function getRecipe(id) {
   try {
     let recipe = await fetch(
       `https://api.spoonacular.com/recipes/${id}/information?apiKey=9ef8ed83e4164a2eb14cf5bff7669c9a&includeNutrition=false`
     );
 
-    let data = recipe.json();
+    let data = await recipe.json();
     return data;
 
     console.log(data);
@@ -23,11 +24,13 @@ async function getRecipe(id) {
     console.log(error);
   }
 }
-window.addEventListener("DOMContentLoaded", () => {
-  getRecipe(4632).then((recipe) => {
-    console.log(recipe.instructions);
-  });
-});
+
+// window.addEventListener("DOMContentLoaded", () => {
+//   getRecipe(4632).then((recipe) => {
+//     console.log(recipe.instructions);
+//   });
+// });
+
 const main = document.querySelector("main");
 const form = document.getElementById("ingredientForm");
 const section = document.querySelector(".ingredient-form");
@@ -84,18 +87,18 @@ form.addEventListener("submit", function (event) {
     //numForm.action = "result.html";
   }
 });
+
 let cookSection = document.createElement("section");
 cookSection.id = "cookSection";
 main.appendChild(cookSection);
+let ids = [];
 
 numForm.addEventListener("submit", () => {
-  console.log(numForm);
   let string = "";
   event.preventDefault();
   if (numForm.querySelector("#protein")) {
     const numOfProtein = numForm.querySelector("#protein").value;
     string += `minProtein=${numOfProtein}&`;
-    console.log(numOfProtein);
   }
   if (numForm.querySelector("#carbohydrate")) {
     const numOfCarbohydrate = numForm.querySelector("#carbohydrate").value;
@@ -105,35 +108,35 @@ numForm.addEventListener("submit", () => {
     const numOfFat = numForm.querySelector("#fat").value;
     string += `minFat=${numOfFat}&`;
   }
-  console.log(string);
-  let ids = [];
 
   getCook(string).then((recipes) => {
-    for (let i = 0; i < 5; i++) {
+    for (let i = 0; i < 3; i++) {
       let div = document.createElement("div");
-      ids[i] = recipes[i].id;
+      console.log(recipes[i].id);
+      ids.push(recipes[i].id);
+      //ids[i] = recipes[i].id;
       div.id = `no${i}-cook`;
       div.innerHTML = `<h3 class="propertyOfRecipe">${recipes[i].title}</h3>
       <img class="propertyOfRecipe" src="${recipes[i].image}"/>
       <span class="propertyOfRecipe">Calories:${recipes[i].calories} Protein: ${recipes[i].protein} Carbs: ${recipes[i].carbs} fat:${recipes[i].fat}</span>
      `;
-
       cookSection.appendChild(div);
     }
+    for (let i = 0; i < 3; i++) {
+      getRecipe(ids[i]).then((info) => {
+        let span = document.createElement("span");
+        span.id = "recipeInfo";
+        if (info.instructions) {
+          span.innerText = info.instructions;
+
+          document.getElementById(`no${i}-cook`).appendChild(span);
+        } else {
+          span.innerText = "No Recipe";
+          document.getElementById(`no${i}-cook`).appendChild(span);
+        }
+      });
+    }
   });
-
-  for (let i = 0; i < 5; i++) {
-    let id = ids[i];
-    getRecipe(id).then((info) => {
-      console.log(info);
-
-      let span = document.createElement("span");
-      span.innerText = info.instructions;
-      console.log(span);
-
-      //document.getElementById(`no${i}-cook`).appendChild(span);
-    });
-  }
 });
 
 function makeInput(nut) {
